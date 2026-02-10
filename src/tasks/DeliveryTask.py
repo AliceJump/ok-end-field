2582c0a66b25a632489de96c8a0b5ec3b160bc25
AliceJump <2630923991@qq.com>
2026-02-10 19:32:52 +0800

    feat: add delivery task with optimized navigation

---
+++ src/tasks/DeliveryTask.py
@@ -0,0 +1,47 @@
+import re
+import time
+
+from src.tasks.BaseEfTask import BaseEfTask
+
+on_zip_line_stop = re.compile("向目标移动")
+continue_next = re.compile("下一连接点")
+
+
+class DeliveryTask(BaseEfTask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.default_config = {"_enabled": True}
+        self.name = "半自动送货"
+        self.description = '选择后上滑索时启动'
+        self.default_config.update({
+            '说明':'需填写滑索分叉序列(例如"108,64,109",是指上滑索后找108m的滑索并试图滑向它,后面依次为第一个分叉点,第二个分叉点...)',
+            '物资回收站左下': "108,64,109,60",
+            '物资回收站': "108,64,109",
+            '武陵城深处': "108,64,108,59",
+            '武陵城右边': "108,106",
+            '选择送货对象': "物资回收站左下",
+        })
+        self.config_type["选择送货对象"] = {'type': "drop_down",
+                                            'options': ['物资回收站左下', '物资回收站', '武陵城深处','武陵城右边']}
+        self.lv_regex = re.compile(r"(?i)lv|\d{2}")
+        self.last_target = None
+
+    def zip_line_list_go(self, zip_line_list):
+        for zip_line in zip_line_list:
+            self.align_ocr_target_to_center(re.compile(str(zip_line)))
+            self.log_info(f"成功将滑索调整到{zip_line}的中心")
+            self.click(after_sleep=0.5)
+            start = time.time()
+            while not self.ocr(match=on_zip_line_stop, box="bottom", log=True):
+                self.send_key("e")
+                if time.time() - start > 60:
+                    raise Exception("滑索超时，强制退出")
+        self.sleep(1)
+        self.click(key="right")
+
+    def run(self):
+        while not self.ocr(match=on_zip_line_stop, box="bottom", log=True):
+            self.sleep(2)
+        zip_line_list_str=self.config.get(self.config.get("选择送货对象"))
+        zip_line_list = [int(i) for i in zip_line_list_str.split(",")]
+        self.zip_line_list_go(zip_line_list)
